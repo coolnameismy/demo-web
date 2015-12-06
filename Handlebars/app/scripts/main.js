97c7cc366bf1aa6e8f2736b6e9746126c6ea9366
coolnameismy <coolnameismy@hotmail.com>
2015-12-06 15:31:44 +0800

    m

--- Handlebars/app/scripts/main.js
+++ Handlebars/app/scripts/main.js
@@ -3,17 +3,16 @@ define(function(require, exports, module) {
 
 	// 通过 require 引入依赖
 	// var $ = require('jquery');
-	// var Handlebars = require('handlebars')
+	var Handlebars = require('handlebars');
 	// var demoTpl = require('../component/Tpl/demoList.handlebars');
  
-
  	
 	$(function(){
 		
 		var tpl = require("./data.tpl");
     	var demoTplc = Handlebars.compile(tpl);
     	$("body").html( demoTplc("hello world"));
-		// console.log(Handlebars);
+	    console.log(Handlebars);
 		console.log("page load complete");
 
 	});
